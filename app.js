a08f2d84638558fe6bda0d2fe8a4c04b2a25afdb
wangenyong <wangenyong@gmail.com>
2018-04-23 11:25:50 +0800

    Set docker.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const sportsRoutes = require('./api/routes/sports');
 
-mongoose.connect('mongodb://localhost:27017/sport_notes' );
+mongoose.connect('mongodb://mongo:27017/sport_notes' );
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
